ffb26f5c85e687f7ef535a68b6ec6d3c19e5a723
DamoFD <voshall@yahoo.com>
2023-06-29 23:51:31 -0700

    emit date from calendar to schedule

--- resources/js/Components/Calendar.vue
+++ resources/js/Components/Calendar.vue
@@ -1,9 +1,11 @@
 <script setup>
-import {ref} from 'vue'
+import {ref, defineEmits, onMounted} from 'vue'
 import {DatePicker} from 'v-calendar'
 import 'v-calendar/style.css'
 
 const date = ref(new Date())
+
+//List of disabled dates
 const disabledDates = ref([
     {
         repeat: {
@@ -11,9 +13,33 @@ const disabledDates = ref([
         }
     }
 ])
+
+//Emit current date on mount
+onMounted(() => {
+    updateDate(date.value)
+})
+
+const emit = defineEmits(['update-date'])
+
+//Emit date to parent
+const updateDate = (date) => {
+    emit('update-date', date)
+}
 </script>
 
 <template>
-    <DatePicker v-model="date" :disabled-dates="disabledDates" />
+    <h1>Your Business Here</h1>
+    <DatePicker v-model="date" :disabled-dates="disabledDates" :min-date="new Date()" @dayclick="updateDate(date)" />
     <p>{{date}}</p>
 </template>
+
+<style>
+/* Change color of Weekdays */
+.vc-container .vc-weekday-2,
+.vc-container .vc-weekday-3,
+.vc-container .vc-weekday-4,
+.vc-container .vc-weekday-5,
+.vc-container .vc-weekday-6 {
+    color: blue
+}
+</style>
--- resources/js/Pages/Schedule.vue
+++ resources/js/Pages/Schedule.vue
@@ -1,7 +1,15 @@
 <script setup>
+import {ref} from 'vue'
 import Calendar from '@/Components/Calendar.vue'
+
+const selectedDate = ref(null)
+
+const updateDate = (date) => {
+    selectedDate.value = date
+}
 </script>
 
 <template>
-    <Calendar />
+    <Calendar @update-date="updateDate" />
+    <p>{{ selectedDate }}</p>
 </template>
