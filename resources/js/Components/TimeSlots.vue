96e87e9e629202b8d68b3346aaa5f6cb9b901480
DamoFD <voshall@yahoo.com>
2023-06-30 00:53:27 -0700

    time slots beginning layout

--- resources/js/Components/TimeSlots.vue
+++ resources/js/Components/TimeSlots.vue
@@ -13,6 +13,14 @@ const formattedDate = computed(() => {
 <template>
     <!-- Main Wrapper -->
     <div class="flex flex-col justify-center items-center w-1/2">
-        <h2 class="text-xl font-nunito font-black">{{ formattedDate }}</h2>
+        <h2 class="text-xl font-nunito font-black text-gray-700">{{ formattedDate }}</h2>
+        <p class="text-md font-inter">What time works best?</p>
+
+        <!-- Time Slots -->
+        <div class="flex flex-col items-center w-full p-4">
+            <div class="border border-gray-300 w-full h-10 flex items-center justify-center rounded-xl">
+                <h3 class="font-inter">9:00 am</h3>
+            </div>
+        </div>
     </div>
 </template>
