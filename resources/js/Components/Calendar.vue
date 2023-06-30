cd4082a826b16f769ecabddc478e6f320a669f4c
DamoFD <voshall@yahoo.com>
2023-06-29 23:08:31 -0700

    created calendar component

--- resources/js/Components/Calendar.vue
+++ resources/js/Components/Calendar.vue
@@ -4,9 +4,16 @@ import {DatePicker} from 'v-calendar'
 import 'v-calendar/style.css'
 
 const date = ref(new Date())
+const disabledDates = ref([
+    {
+        repeat: {
+            weekdays: [1, 7],
+        }
+    }
+])
 </script>
 
 <template>
-    <DatePicker v-model="date" />
+    <DatePicker v-model="date" :disabled-dates="disabledDates" />
     <p>{{date}}</p>
 </template>
