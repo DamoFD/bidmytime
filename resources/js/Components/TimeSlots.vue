0b0791d25950afeea88ac9115531176e6e52bdf5
DamoFD <voshall@yahoo.com>
2023-06-30 10:51:45 -0700

    calculate when bid will end

--- resources/js/Components/TimeSlots.vue
+++ resources/js/Components/TimeSlots.vue
@@ -3,11 +3,32 @@ import {ref, computed} from 'vue'
 import moment from 'moment'
 
 const props = defineProps(['selectedDate'])
+let timeSlots = ref(['9:00 am', '10:00 am', '11:00 am', '12:00 pm', '1:00 pm', '2:00 pm', '3:00 pm', '4:00 pm', '5:00 pm'])
 
 //Format Date
 const formattedDate = computed(() => {
-    return moment(props.selectedDate).format('MMMM Do, YYYY');
+    return moment(props.selectedDate).format('MMMM Do, YYYY')
 })
+
+// Time until bid ends
+const timeUntilBidEnds = (slot) => {
+    // Create moment object for the selected date
+    let selectedDateTime = moment(props.selectedDate);
+
+    // Create moment object for the slot time on the selected date
+    let slotDateTime = moment(`${formattedDate.value} ${slot}`, 'MMMM Do, YYYY h:mm a');
+
+    // Calculate the bid end time
+    let bidEndTime = slotDateTime.clone().subtract(1, 'hours');
+
+    // Calculate the time until the bid ends
+    let duration = moment.duration(bidEndTime.diff(moment()));
+
+    // Return the time until the bid ends, humanized
+    return duration.humanize(true);
+}
+
+
 </script>
 
 <template>
@@ -18,8 +39,13 @@ const formattedDate = computed(() => {
 
         <!-- Time Slots -->
         <div class="flex flex-col items-center w-full p-4">
-            <div class="border border-gray-300 w-full h-10 flex items-center justify-center rounded-xl">
-                <h3 class="font-inter">9:00 am</h3>
+            <div
+                v-for="slot in timeSlots"
+                :key="slot"
+                class="border border-gray-300 w-full h-10 flex items-center justify-center rounded-xl"
+            >
+                <h3 class="font-inter">{{ slot }}</h3>
+                <p>{{ timeUntilBidEnds(slot) }}</p>
             </div>
         </div>
     </div>
