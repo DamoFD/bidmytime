577d19ceeb1089eb97979d9b5f14de903b2dd63e
DamoFD <voshall@yahoo.com>
2023-07-01 01:23:51 -0700

    available weekdays seeder

---
+++ database/seeders/AvailableWeekdaysSeeder.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class AvailableWeekdaysSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        foreach (range(2, 6) as $weekday) {
+            \App\Models\AvailableWeekdays::create([
+                'seller_id' => 1,
+                'day_of_week' => $weekday,
+            ]);
+        }
+    }
+}
