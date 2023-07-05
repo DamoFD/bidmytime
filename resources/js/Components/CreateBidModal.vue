c944594e644ad8227cce1a9fa5fd5399d42fa1e2
DamoFD <voshall@yahoo.com>
2023-07-05 15:57:47 -0700

    create bid modal

---
+++ resources/js/Components/CreateBidModal.vue
@@ -0,0 +1,24 @@
+<script setup>
+import {useForm} from '@inertiajs/vue3'
+
+const emit = defineEmits(['close'])
+
+const form = useForm({
+    amount: null,
+})
+
+const closeModal = () => {
+    form.reset()
+    emit('close')
+}
+
+</script>
+
+<template>
+    <div class="absolute top-0 left-0 flex justify-center items-center w-full h-full bg-black bg-opacity-10">
+        <div class="flex flex-col items-center justify-center bg-white">
+            <p @click="closeModal()" class="p-10 cursor-pointer">Close</p>
+            <h1 class="p-10">This is a Modal</h1>
+        </div>
+    </div>
+</template>
--- resources/js/Pages/Bids/Show.vue
+++ resources/js/Pages/Bids/Show.vue
@@ -1,6 +1,8 @@
 <script setup>
 import moment from 'moment'
-import {computed} from "vue";
+import {computed, ref} from "vue";
+import CreateBidModal from "@/Components/CreateBidModal.vue";
+
 const props = defineProps({
     bids: Array,
     selectedDate: String,
@@ -10,6 +12,7 @@ const props = defineProps({
 
 const sellerName = props.bids[0].seller.name
 const totalBids = props.bids.length
+let showCreateBidModal = ref(false)
 
 // format dates
 const formatCreatedAt = (date) => {
@@ -28,25 +31,29 @@ let highestBid = props.bids.reduce((highest, bid) => {
     return Math.max(highest, bid.amount);
 }, 0);
 
-console.log(props.bids)
-
-
 </script>
 
 <template>
     <div class="w-full min-h-screen bg-gray-300 flex justify-center items-center">
-        <div class="w-full bg-white rounded-lg shadow-lg p-4 flex-1 flex-col flex mx-10">
+        <div class="w-full bg-white rounded-lg shadow-lg p-4 flex-col items-center flex mx-10">
         <h1>Bid on this time slot</h1>
             <p>Timeslot: {{formattedTimeslot}}</p>
         <h2>{{sellerName}}</h2>
         <p>This is your time slot description</p>
         <p>Time left: 1 Hour</p>
-        <p>Current Bid: ${{highestBid}} [{{totalBids}} bids]</p>
         <p>Previous bids:</p>
         <div v-for="bid in bids" :key="bid.id">
             <p>{{ bid.user_id }}: ${{ bid.amount }}</p>
             <p>{{formatCreatedAt(bid.created_at)}}</p>
         </div>
+            <div class="w-1/2 bg-neutral-700 rounded-lg flex justify-between items-center shadow-lg">
+                <div class="text-gray-200 p-6">
+                    <p class="pb-2">Highest Bid</p>
+                    <p>${{highestBid}} [{{totalBids}} bids]</p>
+                </div>
+                <p @click="showCreateBidModal = true" class="text-neutral-700 bg-yellow-200 p-2 mr-6 rounded-lg cursor-pointer">Place a Bid</p>
+            </div>
         </div>
     </div>
+    <CreateBidModal v-if="showCreateBidModal" @close="showCreateBidModal = false" />
 </template>
