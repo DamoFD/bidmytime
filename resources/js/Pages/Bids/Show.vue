9b6a54ed267f04117efcbf94f69bf6fabda35dda
DamoFD <voshall@yahoo.com>
2023-07-05 15:25:34 -0700

    show selected time slot, seller, current bid, amount of bids, and previous bids

--- resources/js/Pages/Bids/Show.vue
+++ resources/js/Pages/Bids/Show.vue
@@ -1,17 +1,52 @@
 <script setup>
+import moment from 'moment'
+import {computed} from "vue";
 const props = defineProps({
-    bids: Array
+    bids: Array,
+    selectedDate: String,
+    startTime: String,
+    endTime: String,
 })
 
+const sellerName = props.bids[0].seller.name
+const totalBids = props.bids.length
+
+// format dates
+const formatCreatedAt = (date) => {
+    return moment(date).format('MMMM Do, YYYY [at] hh:mm a')
+}
+
+// format timeslot
+const formattedTimeslot = computed(() => {
+    const startDate = moment(`${props.selectedDate} ${props.startTime}`, 'YYYY-MM-DD h:mm a')
+    const endDate = moment(`${props.selectedDate} ${props.endTime}`, 'YYYY-MM-DD h:mm a')
+    return `${startDate.format('MMMM Do, YYYY h:mm a')} - ${endDate.format('h:mm a')}`
+})
+
+// Find the highest bid
+let highestBid = props.bids.reduce((highest, bid) => {
+    return Math.max(highest, bid.amount);
+}, 0);
+
 console.log(props.bids)
+
+
 </script>
 
 <template>
-    <div>
-        <h1>Bids</h1>
+    <div class="w-full min-h-screen bg-gray-300 flex justify-center items-center">
+        <div class="w-full bg-white rounded-lg shadow-lg p-4 flex-1 flex-col flex mx-10">
+        <h1>Bid on this time slot</h1>
+            <p>Timeslot: {{formattedTimeslot}}</p>
+        <h2>{{sellerName}}</h2>
+        <p>This is your time slot description</p>
+        <p>Time left: 1 Hour</p>
+        <p>Current Bid: ${{highestBid}} [{{totalBids}} bids]</p>
+        <p>Previous bids:</p>
         <div v-for="bid in bids" :key="bid.id">
             <p>{{ bid.user_id }}: ${{ bid.amount }}</p>
-            <p>{{bid.seller.name}}</p>
+            <p>{{formatCreatedAt(bid.created_at)}}</p>
+        </div>
         </div>
     </div>
 </template>
